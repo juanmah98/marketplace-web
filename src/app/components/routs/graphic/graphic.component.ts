930503886b5722492bf386d896ce254ab55ea184
JuanmaH198 <juanmah1998@gmail.com>
2022-12-06 08:34:57 -0300

    response chart

--- src/app/components/routs/graphic/graphic.component.ts
+++ src/app/components/routs/graphic/graphic.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { GraphicService } from 'src/app/services/graphic.service';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { LegendPosition } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-graphic',
@@ -13,19 +15,22 @@ export class GraphicComponent implements OnInit {
 
   // options
   legend: boolean = true;
+  legendTitle: string = 'Chart data'
+  legendPosition = LegendPosition.Right;
   showLabels: boolean = true;
   animations: boolean = true;
   xAxis: boolean = true;
   yAxis: boolean = true;
   showYAxisLabel: boolean = true;
   showXAxisLabel: boolean = true;
-  
+  customColors = '#E44D25';
   autoScale: boolean = true;
   xAxisLabel: string = 'Year';
   yAxisLabel: any = 'Flowkai';
   color ='#5AA454';
+
  
-  timeline: boolean = false;
+  timeline: boolean = true;
   
   
 
@@ -33,8 +38,59 @@ export class GraphicComponent implements OnInit {
     domain: ['#5AA454', '#E44D25', '#23A6F0', '#a8385d', '#a8385d', '#aae3f5']
   };
 
-  constructor(private graphicServices: GraphicService) {
+/*   mdq: MediaQueryList;
+  mediaQueryListener:()=>void; */
+
+  constructor(private graphicServices: GraphicService,  changeDetectorRef: ChangeDetectorRef, media: MediaMatcher ) {
    /*  Object.assign(this, { multi }); */
+   
+   setTimeout(() => {
+   this.tamVentana()
+   },1)
+
+   
+  }
+
+ tamVentana() {
+    var tam: [number, number]  = [0, 0];
+    if (typeof window.innerWidth != 'undefined')
+    {
+      tam = [window.innerWidth / 1.6, window.innerHeight /  1.6];
+    }
+    else if (typeof document.documentElement != 'undefined'
+        && typeof document.documentElement.clientWidth !=
+        'undefined' && document.documentElement.clientWidth != 0)
+    {
+      tam = [
+         
+        document.documentElement.clientWidth ,
+          document.documentElement.clientHeight 
+      ];
+    }
+    else   {
+      tam = [
+         
+        document.getElementsByTagName('body')[0].clientWidth ,
+          document.getElementsByTagName('body')[0].clientHeight
+      ];
+    }
+    this.view = tam;
+    if(tam[0] < 800){
+      this.showXAxisLabel = false
+      this.showYAxisLabel = false
+      this.legendPosition = LegendPosition.Below;
+    }else{
+      this.showXAxisLabel = true
+      this.showYAxisLabel = true
+      this.legendPosition = LegendPosition.Right;
+    }
+    return tam;
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.tamVentana()
+      this.ngOnInit()
+      },1000)
   }
 
  get multi(){
@@ -57,8 +113,6 @@ export class GraphicComponent implements OnInit {
     console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
-  ngOnInit(): void {
-  }
 
  
 
