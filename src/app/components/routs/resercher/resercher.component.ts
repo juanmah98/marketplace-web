fbd6289576751e12412a907ebb29853225100fae
JuanmaH198 <juanmah1998@gmail.com>
2023-05-30 18:23:22 +0200

    calcando figma

--- src/app/components/routs/resercher/resercher.component.ts
+++ src/app/components/routs/resercher/resercher.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-resercher',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resercher.component.css']
 })
 export class ResercherComponent implements OnInit {
-
+  @ViewChild('carouselElement', { static: true })
+  carouselElement!: ElementRef;
   constructor() { }
+  activeItemIndex = 0;
+  totalItems = 5;
 
   ngOnInit(): void {
   }
 
+
+  prevSlide() {
+    this.activeItemIndex = (this.activeItemIndex - 1 + this.totalItems) % this.totalItems;
+    
+  }
+
+  nextSlide() {
+    this.activeItemIndex = (this.activeItemIndex + 1) % this.totalItems;
+    console.log("this.activeItemIndex")
+    console.log(this.activeItemIndex)
+  }
+
+  activateCarouselItem(itemIndex: number) {
+    this.activeItemIndex = itemIndex;
+    const carouselItems = this.carouselElement.nativeElement.querySelectorAll('.carousel-item');
+    carouselItems.forEach((item: HTMLElement, index: number) => {
+      if (index === itemIndex) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
+  }
 }
--- src/app/components/routs/users/users.component.ts
+++ src/app/components/routs/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  @ViewChild('carouselElement', { static: true })
+  carouselElement!: ElementRef; 
   constructor() { }
-
+  activeItemIndex = 0;
+  totalItems = 4;
   ngOnInit(): void {
+  }
+
+  prevSlide() {
+    this.activeItemIndex = (this.activeItemIndex - 1 + this.totalItems) % this.totalItems;
+    
+  }
+
+  nextSlide() {
+    this.activeItemIndex = (this.activeItemIndex + 1) % this.totalItems;
+    console.log("this.activeItemIndex")
+    console.log(this.activeItemIndex)
+  }
+
+  activateCarouselItem(itemIndex: number) {
+    this.activeItemIndex = itemIndex;
+    const carouselItems = this.carouselElement.nativeElement.querySelectorAll('.carousel-item');
+    carouselItems.forEach((item: HTMLElement, index: number) => {
+      if (index === itemIndex) {
+        item.classList.add('active');
+      } else {
+        item.classList.remove('active');
+      }
+    });
   }
 
 }
