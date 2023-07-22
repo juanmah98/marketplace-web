2b49df0a5a51f68eae172649bc94ca7ce72c20d5
JuanmaH198 <juanmah1998@gmail.com>
2023-07-22 10:53:04 +0200

    links apra out

--- src/app/services/apra.service.ts
+++ src/app/services/apra.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class ApraService {
 
-  baseURL: string ='https://flowkai.pythonanywhere.com';
+  baseURL: string ='http://flowkai.pythonanywhere.com';
   
 
   constructor(private http: HttpClient) { }
